d34878d1b894bd959090be74b4a6b483bc40bc60
vtjvlad <vtj.vlad@gmail.com>
2025-04-05 11:28:47 +0300

    Delete routes directory

--- routes/orderRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const orderController = require('../controllers/orderController');
-const { verifyToken } = require('../middlewares/authMiddleware');
-
-router.post('/', verifyToken, orderController.createOrder);
-router.get('/user', verifyToken, orderController.getOrdersByUser);
-
-module.exports = router;
