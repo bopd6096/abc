bed7a52c551cbda7b67629ba09cfb7dae486a705
vtjvlad <vtj.vlad@gmail.com>
2025-04-06 10:44:05 +0300

    Save server.js.

--- server.js
+++ server.js
@@ -285,25 +285,7 @@ app.get('/protected', authenticateToken, (req, res) => {
 
 // Определение схемы для товара
 const productSchema = new mongoose.Schema({
-    //   id: { type: String },  // ID товара
-    //   name: { type: String },  // Название товара
-    //   description: { type: String },  // Описание товара
-    //   count: { type: String },  // Количество
-    //   preview_link: { type: String },  // Ссылка на изображение
-    //   send_preview_as_document: { type: String },
-    //   article: { type: String },  // Артикул
-    //   enabled: { type: String },  // Включен/выключен
-    //   category: { type: String },  // Категория
-    //   purchase_success_command: { type: String },
-    //   promocodes: { type: String },  // Промокоды
-    //   promocodes_variable: { type: String },
-    //   promocodes_purchase_success_command: { type: String },
-    //   promocodes_multiple: { type: String },
-    //   promocodes_random_order: { type: String },
-    //   is_parent: { type: String },
-    //   group_id: { type: String },  // ID группы
-    //   UAH: { type: String }  // Цена в UAH
-    // });
+   
     links: {
         url: {
             type: String
